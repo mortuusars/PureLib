47c83cd7e0a81fa9bb0bd5f1cc11211d5787c664
mortuusars <mortuusars@gmail.com>
2022-04-25 00:40:33 +0300

    1.1.2 - added Long to readable size extension,

---
+++ PureLib.Tests/FileSizeTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace PureLib.Tests;
+
+
+public class FileSizeTests
+{
+    [Theory]
+    [InlineData(1, "1 B")]
+    [InlineData(1025, "1 KB")]
+    [InlineData(1500000, "1.4 MB")]
+    [InlineData(-1500000, "-1.4 MB")]
+    [InlineData(-10000500000, "-9.3 GB")]
+    public void ToSizeTests(long input, string expected)
+    {
+        Assert.Equal(expected, input.ToSize());
+    }
+}
---
+++ PureLib/ExtensionMethods/FileSizeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureLib;
+
+public static class FileSizeExtensions
+{
+    /// <summary>
+    /// Converts number of bytes to the corresponding size in parts of 1024. KB, MB, GB, etc.
+    /// </summary>
+    /// <param name="bytes">Total number of bytes. Can have negative value.</param>
+    /// <param name="bibytes">Specifies whether to use KiB, MiB, etc - instead of KB, MB.</param>
+    /// <returns>Human readable size value - 50 KB, 1.4 GB, etc. If result is in B or KB - it will be rounded to whole number. Otherwise - to the 1st decimal number.</returns>
+    public static string ToSize(this long bytes, bool bibytes = false)
+    {
+        decimal size = bytes;
+        int numberOfDivisions = 0;
+
+        while (Math.Abs(size) > 1024)
+        {
+            size /= 1024;
+            numberOfDivisions++;
+        }
+
+        // No decimal part if KB.
+        decimal number = numberOfDivisions == 1 ? Math.Round(size, 0) : Math.Round(size, 1);
+
+        string[] chars = bibytes ?
+            new string[] { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB" } :
+            new string[] { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+        
+        return $"{number} {chars[numberOfDivisions]}";
+    }
+}
